590af91ac08551524bcc6d1f019fb456a59944d0
prabhatsih <prabhats1410@gmail.com>
2023-06-23 14:54:43 +0530

    msg2

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
 //Traversing
 //Moving from Parent to Child or Child to parent elemnt is called Traversing.
+//We must have a child parent structure for traversing.  
+
+let prabhat=(e)=>{
+    alert(e.closest('tr').querySelector('td:nth-child(2)'));
+}
+
+document.querySelector('.p_viewbtn').addEventListener('click', (e) =>{ //e is a formal argument.
+    console.log(e.target.closest('tr').querySelector('td:nth-child(2').innerHTML); //Closest() is mailny used for traversing up.
+    //QuerySelector is a traversing down method.
+})
